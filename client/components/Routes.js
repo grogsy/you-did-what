4b028d6c77aec66fe9aa48b8f1a96b1b77014849
Brian <a.gray.cloud@gmail.com>
2020-01-11 06:08:26 -0400

    implement single task  view

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import TaskList from "./task/TaskList";
 import TaskForm from "./task/TaskForm";
+import SingleTask from "./task/SingleTask";
+// const TaskList = lazy(() => import("./task/TaskList"));
+// const SingleTask = lazy(() => import("./task/SingleTask"));
 
 const Routes = props => {
   return (
     <Switch>
       <Route path="/new" component={TaskForm} />
+      <Route path="/tasks/:id" component={SingleTask} />
       <Route component={TaskList} />
     </Switch>
   );
