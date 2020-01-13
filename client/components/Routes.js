b7a361cc06891020a1d2f3f11f0a9fa21a7bb093
Brian <a.gray.cloud@gmail.com>
2020-01-12 22:04:39 -0400

    add mock component

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -1,16 +1,16 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import TaskList from "./task/TaskList";
 import TaskForm from "./task/TaskForm";
 import SingleTask from "./task/SingleTask";
-// const TaskList = lazy(() => import("./task/TaskList"));
-// const SingleTask = lazy(() => import("./task/SingleTask"));
+import Settings from "./user/Settings";
 
-const Routes = props => {
+const Routes = () => {
   return (
     <Switch>
+      <Route exact path="/tasks/:id" component={SingleTask} />
       <Route path="/new" component={TaskForm} />
-      <Route path="/tasks/:id" component={SingleTask} />
+      <Route path="/settings" component={Settings} />
       <Route component={TaskList} />
     </Switch>
   );
