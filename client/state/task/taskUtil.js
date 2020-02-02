92ae4090675b01c0affe438e2288286e2a2b916c
Brian <a.gray.cloud@gmail.com>
2020-02-02 02:12:20 -0400

    add a util file for task state

--- client/state/task/taskUtil.js
+++ client/state/task/taskUtil.js
@@ -1,8 +1,27 @@
-export const parseDate = date => {
-  //   const fromPgTimestamp = Date(date.replace(" ", "T"));
+const TZ_LOCALE = "default";
+const TIME_OPTS = {
+  month: "2-digit",
+  day: "2-digit",
+  year: "numeric"
+  // minute: "numeric",
+  // hour: "numeric"
+};
+const ABBR_TIME_OPTS = {
+  weekday: "long",
+  month: "short",
+  day: "2-digit",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric"
+};
 
-  const fromPgTimestamp = new Date(date.replace(" ", "T")).toLocaleString();
-  return fromPgTimestamp;
+export const parseDate = (date, long = false) => {
+  if (long) {
+    return new Date(date).toLocaleString(TZ_LOCALE, ABBR_TIME_OPTS);
+  }
+  return new Date(date.replace(" ", "T")).toLocaleString(TZ_LOCALE, TIME_OPTS);
+
+  //   const fromPgTimestamp = Date(date.replace(" ", "T"));
   //   console.log(fromPgTimestamp);
   //   const month = "" + (fromPgTimestamp.getMonth() + 1);
   //   //   console.log(month);
@@ -10,4 +29,15 @@ export const parseDate = date => {
   //   //   console.log(day);
   //   //   return fromPgTimestamp;
   //   return month;
+};
+
+export const giveDateFields = task => {
+  task.posted = parseDate(task.createdAt);
+  task.posted_long = parseDate(task.createdAt, true);
+  if (task.status === "Completed") {
+    task.completedAt = parseDate(task.updatedAt);
+    task.completedAt_long = parseDate(task.updatedAt, true);
+  }
+
+  return task;
 };
