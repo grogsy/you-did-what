9fe08d119e327ba680c8c1886c7b3d29cb1b32f3
Brian <a.gray.cloud@gmail.com>
2020-02-05 06:31:39 -0500

    implement resources

--- client/components/task/SingleTask/index.js
+++ client/components/task/SingleTask/index.js
@@ -1,17 +1,22 @@
 import React, { useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { useDispatch, useSelector } from "react-redux";
 
-import TaskResources from "./TaskResources";
-import ResourcesList from "./ResourcesList";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import TaskHeader from "./TaskHeader";
+import TaskDescription from "./TaskDescription";
+import ResourcesList from "../../resource/ResourceList";
+import TaskTags from "./TaskTags";
+
 import { getSingleTask, cleanupSingleTask } from "../../../state";
+import { cleanedupResources } from "../../../state/resource/resourceActions";
 
 const SingleTaskContainer = props => {
   const taskId = props.match.params.id;
   const dispatch = useDispatch();
   const task = useSelector(state => state.tasks.viewedTask);
+  const resources = useSelector(state => state.resources.resources);
 
   useEffect(() => {
     dispatch(getSingleTask(taskId));
@@ -21,26 +26,30 @@ const SingleTaskContainer = props => {
     };
   }, [dispatch]);
 
+  useEffect(() => {
+    return function cleanup() {
+      dispatch(cleanedupResources());
+    };
+  }, [dispatch]);
+
   return (
     <Container className="mt-3">
       <Row>
-        <Col>
-          <h3>Task</h3>
-          <p>{task.name}</p>
-        </Col>
-        <Col>
-          <h3>Created</h3>
-          <abbr title={task.posted_long}>{task.posted}</abbr>
-        </Col>
+        <TaskHeader
+          name={task.name}
+          longDate={task.posted_long}
+          shortDate={task.posted}
+        />
       </Row>
+      <hr />
       <Row>
-        <Col>
-          <h3>Description</h3>
-          <p>{task.description}</p>
-        </Col>
+        <TaskDescription description={task.description} taskId={taskId} />
+        <TaskTags tags={task.tags} />
       </Row>
-      {/* <TaskResources taskId={taskId} /> */}
-      <ResourcesList taskId={taskId} />
+      <hr />
+      <Row>
+        <ResourcesList taskId={taskId} resources={resources} />
+      </Row>
     </Container>
   );
 };
