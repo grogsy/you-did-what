ab2f94e426540952e96978c9838325e703cd5359
Brian <a.gray.cloud@gmail.com>
2020-02-09 05:09:48 -0500

    add category to single task view header

--- client/components/task/SingleTask/TaskHeader.js
+++ client/components/task/SingleTask/TaskHeader.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-const TaskHeader = ({ name, longDate, shortDate }) => {
+const TaskHeader = ({ name, category, longDate, shortDate }) => {
   return (
     <Col>
-      <h1>
-        <em>{name}</em>
-      </h1>
-      Created:{" "}
-      <small>
-        <abbr title={longDate}>{shortDate}</abbr>
-      </small>
+      <Row>
+        <Col>
+          <h1>
+            <em>{name}</em>
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          Created: &nbsp;
+          <small className="my-auto">
+            <abbr title={longDate}>{shortDate}</abbr>
+          </small>
+          <div className="float-right">
+            Under: &nbsp;
+            <small className="my-auto">
+              <em>
+                <a href="#">{category}</a>
+              </em>
+            </small>
+          </div>
+        </Col>
+      </Row>
     </Col>
   );
 };
