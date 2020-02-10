3cf6e1a229f73bb164a17ea16dc0fb9c1dcc34b5
Brian <a.gray.cloud@gmail.com>
2020-02-10 01:35:18 -0500

    use layout component

--- client/components/index.js
+++ client/components/index.js
@@ -2,28 +2,38 @@ import React, { lazy, Suspense } from "react";
 import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
 
+import Layout from "./Layout";
+
 const Routes = lazy(() => import("./Routes"));
-import Navbar from "./Navbar";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Container style={style.container} fluid>
-        <Suspense fallback={<Spinner variant="info" animation="border" />}>
-          <Routes />
-        </Suspense>
-      </Container>
-    </>
+    <Layout>
+      <Routes />
+    </Layout>
   );
 };
+// import Navbar from "./Navbar";
 
-const style = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  }
-};
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Container style={style.container} fluid>
+//         <Suspense fallback={<Spinner variant="info" animation="border" />}>
+//           <Routes />
+//         </Suspense>
+//       </Container>
+//     </>
+//   );
+// };
+
+// const style = {
+//   container: {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center"
+//   }
+// };
 
 export default App;
