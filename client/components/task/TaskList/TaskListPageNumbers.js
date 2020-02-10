23e273d1e49ef75f5261e4b5605a3ecce10c8b12
Brian <a.gray.cloud@gmail.com>
2020-02-10 04:29:45 -0500

    wrap pagination component in column div

--- client/components/task/TaskList/TaskListPageNumbers.js
+++ client/components/task/TaskList/TaskListPageNumbers.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import Col from "react-bootstrap/Col";
 
 const TaskListPageNumbers = ({ pages, handleOnClick }) => {
   return (
-    <>
+    <Col>
       {pages.map((_, index) => (
         <Button
           className="mx-1"
           key={index}
           onClick={() => handleOnClick(index)}
           variant="secondary"
+          title={`Go to page ${index + 1}`}
         >
           {index + 1}
         </Button>
       ))}
-    </>
+    </Col>
   );
 };
 
